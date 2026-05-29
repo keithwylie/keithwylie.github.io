df9fa7187759693824f136d778403c6f58b46bd5
keithwylie <keithwylie@users.noreply.github.com>
2026-05-29 17:10:50 +0000

    Deploying to gh-pages from @ keithwylie/keithwylie.github.io@e8e6f66659cb2af7479aa5ef28934c0da666c004 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "keithwylie/keith",
+    "data-repo": "keithwylie/keithwylie.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
